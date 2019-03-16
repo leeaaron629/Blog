ce2a4c174c620a00cbff7761ff47ec6bc779149f
Aaron Lee <leeaaron326@gmail.com>
2019-03-16 19:19:45 -0400

    Removing un-necessary files

--- Article 1 - Use Guard Clauses/code_snippets_refined.js
+++ Article 1 - Use Guard Clauses/code_snippets_refined.js
@@ -16,22 +16,7 @@ let doComplexConditionalWork = (flag, obj) => {
 	}
 
 	let stats = doWorkWithObject(obj);
-
-	// if (stats != null) {
-	// 	var success = processStats(stats);
-	// } else {
-	// 	console.log('Failed! No work stats from object');
-	// }
-
 	let success = processStats(stats);
-
-	// if (success) {
-	// 	console.log('Work stats persisted in database successfully');
-	// } else {
-	// 	console.log('Work stats not persisted in database, defaulting to log file');
-	// 	writeStatsToFile(stats);
-	// }
-
 	processResultFromStats(success);
 
 }
